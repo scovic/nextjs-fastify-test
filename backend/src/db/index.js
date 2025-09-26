0c1aaa29052d2d2edca00105256cdca2c49c1c47
Stefan <stefan.covic.dev@gmail.com>
2025-09-26 17:48:34 +0200

    Fix filename for knex client

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -1,20 +1,22 @@
 import knex from 'knex';
 
-const knexClient = knex({
-  client: 'sqlite3', // or 'better-sqlite3'
-  connection: {
-    filename: './db.sqlite',
-  },
-});
+// const knexClient = 
 
 export default class DB {
+  static knexClient = knex({
+    client: 'sqlite3', // or 'better-sqlite3'
+    connection: {
+      filename: './dev.sqlite3',
+    },
+  });;
+
   static async addEmail(email) {
-    return knexClient('emails').insert(email)
+    return this.knexClient('emails').insert(email)
   }
 
   static async findEmails(searchString) {
     if (!searchString) return [];
-    return knexClient('emails')
+    return this.knexClient('emails')
       .where(function() {
         this.where('to', 'like', `%${searchString}%`)
           .orWhere('cc', 'like', `%${searchString}%`)
